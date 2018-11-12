f4b3d65a32c799ac23d22003f471d3be238fd3c6
behdad222 <behdad.222@gmail.com>
2018-11-12 14:54:24 +0330

    refactor

--- TapsellSDK_v3_Eclipse/src/ir/tapsell/samplev3/MainActivity.java
+++ TapsellSDK_v3_Eclipse/src/ir/tapsell/samplev3/MainActivity.java
@@ -19,28 +19,26 @@ import ir.tapsell.sdk.TapsellShowOptions;
 
 public class MainActivity extends Activity {
 
-    private static final String appKey = "kilkhmaqckffopkpfnacjkobgrgnidkphkcbtmbcdhiokqetigljpnnrbfbnpnhmeikjbq";
+    private static final String ZONE_ID = "5a5dbd5cc21bf000010d1686";
 
-    private static final String myAppMainZoneId = "586f52d9bc5c284db9445beb";
-
-    Button requestAdBtn, showAddBtn;
-    TapsellAd ad;
+    private Button requestAdBtn, showAddBtn;
+    private TapsellAd ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
 
-        TapsellConfiguration config = new TapsellConfiguration(this);
-        config.setDebugMode(true);
-        config.setAutoHandlePermissions(true);
-
-        Tapsell.initialize(this, config, appKey);
+        requestAdBtn = (Button) findViewById(R.id.btnRequestAd);
+        showAddBtn = (Button) findViewById(R.id.btnShowAd);
+        
+        showAddBtn.setEnabled(false);
 
         Tapsell.setRewardListener(new TapsellRewardListener() {
             @Override
             public void onAdShowFinished(TapsellAd ad, boolean completed) {
-                Log.e("MainActivity","isCompleted? "+completed);
+                Log.e("MainActivity", "isCompleted? " + completed);
                 // store user
                 new AlertDialog.Builder(MainActivity.this)
                         .setTitle("Info")
@@ -54,32 +52,26 @@ public class MainActivity extends Activity {
                         .show();
             }
         });
-        
-
-        requestAdBtn = (Button) findViewById(R.id.btnRequestAd);
 
         requestAdBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadAd(myAppMainZoneId);
+                loadAd(ZONE_ID);
             }
         });
-
-        showAddBtn = (Button) findViewById(R.id.btnShowAd);
-
-        showAddBtn.setEnabled(false);
-
+        
         showAddBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(ad!=null && ad.isValid())
-                {
+                if(ad != null && ad.isValid()) {
                     showAddBtn.setEnabled(false);
+                    
                     TapsellShowOptions showOptions = new TapsellShowOptions();
                     showOptions.setBackDisabled(false);
                     showOptions.setImmersiveMode(true);
                     showOptions.setRotationMode(TapsellShowOptions.ROTATION_UNLOCKED);
+                    
                     ad.show(MainActivity.this, showOptions, new TapsellAdShowListener() {
 						
 						@Override
@@ -101,34 +93,34 @@ public class MainActivity extends Activity {
 
         TapsellAdRequestOptions options = new TapsellAdRequestOptions(TapsellAdRequestOptions.CACHE_TYPE_STREAMED);
 
-        Tapsell.requestAd(MainActivity.this, zoneId, options, new TapsellAdRequestListener() {
+        Tapsell.requestAd(this, zoneId, options, new TapsellAdRequestListener() {
             @Override
             public void onError(String error) {
-                Log.d("Tapsell Sample","Error: "+error);
+                Log.d("Tapsell Sample", "Error: " + error);
             }
 
             @Override
             public void onAdAvailable(TapsellAd ad) {
 
-                MainActivity.this.ad = ad;
+            	MainActivity.this.ad = ad;
                 showAddBtn.setEnabled(true);
-                Log.d("Tapsell Sample","Ad is available");
+                Log.d("Tapsell Sample", "Ad is available");
             }
 
             @Override
             public void onNoAdAvailable() {
-                Log.d("Tapsell Sample","No ad available");
+                Log.d("Tapsell Sample", "No ad available");
             }
 
             @Override
             public void onNoNetwork() {
-                Log.d("Tapsell Sample","No network");
+                Log.d("Tapsell Sample", "No network");
             }
 
             @Override
             public void onExpiring(TapsellAd ad) {
                 showAddBtn.setEnabled(false);
-                loadAd(myAppMainZoneId);
+                loadAd(ZONE_ID);
             }
         });
 
